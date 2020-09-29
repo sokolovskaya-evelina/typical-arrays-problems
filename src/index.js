d950463cfa7730ebcee9d9ea1566d5c9e4658099
Evelina Sokolovskaya <sokolovskayaevi@gmail.com>
2020-09-29 23:29:50 +0300

    make a task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    let min = array[0];
+    for (var i = 0; i < array.length; i++) {
+      if (min > array[i]) min = array[i]; 
+    } return min;
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    let max = array[0];
+    for (var i = 0; i < array.length; i++) {
+      if (max < array[i]) max = array[i]; 
+    } return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  } else {
+      let sum = array.reduce((a, b) => a + b, 0);
+let result = sum / array.length;
+return result;
+  } 
 }
